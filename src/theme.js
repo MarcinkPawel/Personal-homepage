d3c5e36dda7fbd0bc7c9e2aed057a294f45aaef2
Pawel Marcinkowski <marcinkowski.pwl@gmail.com>
2023-03-21 21:37:00 +0100

    Style-components added, objects with skills/tech

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+    color: {
+        MineShaft: "#252525", //heading content
+        SlateGray: "#6E7E91", //p text
+        White: "#FFFFFF",
+        ScienceBlue: "#0366D6", //blue buttons
+        Mercury: "#E5E5E5", //bg color main
+        Alto: "#DADADA", //social media logo
+    },
+};
