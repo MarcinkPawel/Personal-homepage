3727043a5f570e7aaca190ffa24017c7566a3553
Pawel Marcinkowski <marcinkowski.pwl@gmail.com>
2023-05-31 08:17:40 +0200

    Main with styled added

--- src/features/Main/index.js
+++ src/features/Main/index.js
@@ -2,13 +2,23 @@ import { Section } from "../../common/Section";
 import { About } from "../../common/About";
 import { Portfolio } from "../../common/Portfolio";
 import { Wrapper } from "./styled";
+import { myData } from "../../myData";
+import { Footer } from "../../common/Footer";
 
 export const Main = () => {
-    return (
-        <Wrapper>
-        <About />
-      <Section heading={"czesc"} />
-      <Section heading={"gsdfshgdfhd"} />
+  return (
+    <Wrapper>
+      <About />
+      <Section
+        heading="My skillset includes 🛠️"
+        skills={myData.currentSkills}
+      />
+      <Section
+        heading="What I want to learn next 🚀"
+        skills={myData.skillsToLearn}
+      />
       <Portfolio />
-        </Wrapper>
-    )};
+      <Footer />
+    </Wrapper>
+  );
+};
--- src/features/Main/styled.js
+++ src/features/Main/styled.js
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    display: inherit;
-    background-color: ${({ theme }) => theme.color.white};
-    padding: 119px 0;
-    margin: 0
+  max-width: 1248px;
+  margin: 100px auto 109px;
+  padding: 16px;
 `;
